ac032d12d813194e13122e787a5d68b575209dfb
YukiAma <Krupskiy25.12.00@gmail.com>
2020-08-07 19:51:33 +0300

    Update SysNumMath.cs

--- CalculatorNumSys/SysNumMath.cs
+++ CalculatorNumSys/SysNumMath.cs
@@ -8,9 +8,18 @@ namespace CalculatorNumSys
 {
     class SysNumMath
     {
-        public static void Binary()
+        public static void fromDecimal(int num, int system)
         {
-            
+            int remainder, result;
+            while (num >= system)
+            {
+                result = num / system;
+                remainder = num - (result * system);
+                num = result;
+                Console.Write(remainder);
+                //Console.WriteLine(result);
+            }
+            Console.WriteLine(num);
         }
     }
 }
